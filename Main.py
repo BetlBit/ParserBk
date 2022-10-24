cf273ccf70f7adb65d8d0fe07fd9740f97fed193
BetlBit <inthebetmen@mail.ru>
2022-10-25 02:38:16 +0300

    bruh

--- Main.py
+++ Main.py
@@ -1,16 +1,6 @@
-from itertools import tee
-from turtle import title
 import requests
-from bs4 import BeautifulSoup as bs
-import pandas as pd
+from bs4 import BeautifulSoup
 
-URL_TEMPLATE = "https://vprognoze.ru"
-r = requests.get(URL_TEMPLATE)
-print(r.status_code)
-
-print(r.text)
-
-# soup = bs(r.text, "html.parser")
-# vacancies_names = soup.find_all('div', class_='section__body')
-# for name in vacancies_names:
-#     print(name.a['title'])
+link = "https://vprognoze.ru/user/umqq/"
+responce = requests.get(link).text
+print(responce)
